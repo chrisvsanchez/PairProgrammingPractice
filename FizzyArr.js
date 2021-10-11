ffda11f85d0f3e7f452f26b0045a330b2d11a6d0
chrisvsanchez <chrisvsanchez@gmail.com>
2021-10-11 11:41:38 -0400

    added fizzyarr file

---
+++ FizzyArr.js
@@ -0,0 +1,21 @@
+//Return a string that counts how many vowels are found in the given array and return an array with only number and replace those numbers with Fizz OR Buzz according to divisibility as shown below.
+
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i if non of the above conditions are true.
+
+// input= [1,2,3,4,5,"a","I",6,7,"Z"]  return  "There are 2 vowels and this is the result of fizzbuzz: [1,2,"Fizz",4,"Buzz", "Fizz", 7]"
+// input=  [1,2,3,4,5,"a","i",6,7,8,9,10,11,12,13,14,15,"m","a" "a" "a"]  return  "There are 5 vowels and this is the result of fizzbuzz: [1,2,"Fizz",4,"Buzz", "Fizz", 7,8,"Fizz","Buzz",11,"Fizz",13,14,"Buzz"]"
+// input=   ["a", "E","i","o","U",3,3,3,15,15,15]  return  "There are 5 vowels and this is the result of fizzbuzz: ["Fizz","Fizz","Fizz","FizzBuzz","FizzBuzz","FizzBuzz" "
+let arr1 = [1,2,3,4,5,"a","I",6,7,"Z"]
+let arr2 = [1,2,3,4,5,"a","i",6,7,8,9,10,11,12,13,14,15,"m","a" "a" "a"]
+let arr3 = ["a", "E","i","o","U",3,3,3,15,15,15]
+
+function FizzyVowels(arr){
+
+}
+
+FizzyVowels(arr1)
+FizzyVowels(arr2)
+FizzyVowels(arr3)
